90530f7c0fca3bfeb013b7af5739a3127fe3e4e0
boxing-in-iT <tbol240@gmail.com>
2024-03-19 17:00:40 +0200

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import TableElement from "./components/TableElement/table-element";
 import RatingTable from "./components/RatingTable/rating-table";
-import { useEffect, useState } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [loadedUsers, setLoadedUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // Функция генерации случайного пользователя
     function generateRandomUser() {
       const randomColor = Math.floor(Math.random() * 3);
       const randomName = "User" + Math.floor(Math.random() * 10000);
       const randomSpeed = Math.floor(Math.random() * 20);
       const randomTime = Math.floor(Math.random() * 5000);
 
-      // Форматирование времени в нужный формат
       const minutes = Math.floor(randomTime / 60000);
       const seconds = Math.floor((randomTime % 60000) / 1000);
       const milliseconds = randomTime % 1000;
@@ -31,17 +30,41 @@ function App() {
       };
     }
 
-    // Генерация пользователей и установка их в состояние
     const generatedUsers = [];
     for (let i = 0; i < 1000; i++) {
       generatedUsers.push(generateRandomUser());
     }
     setUsers(generatedUsers);
+    setLoadedUsers(generatedUsers.slice(0, 50));
   }, []);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        setLoading(true);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      const currentLength = loadedUsers.length;
+      const newUsers = users.slice(currentLength, currentLength + 50);
+      setLoadedUsers((prevUsers) => [...prevUsers, ...newUsers]);
+      setLoading(false);
+    }
+  }, [loading, loadedUsers, users]);
 
   return (
     <div className="App">
-      <RatingTable users={users} />
+      <RatingTable users={loadedUsers} />
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
